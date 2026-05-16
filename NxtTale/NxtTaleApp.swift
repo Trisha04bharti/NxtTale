5597d6d1c1dbf9cd376c2c3c0b678fd26659d20e
Trisha Bharti <trishabharti444@gmail.com>
2026-05-16 12:39:29 +0530

    Initial Commit

---
+++ NxtTale/NxtTaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NxtTaleApp.swift
+//  NxtTale
+//
+//  Created by Vikram Kumar on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NxtTaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
