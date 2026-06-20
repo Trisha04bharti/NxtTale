a52d86b8c2b5fc4181a608da043b85ab6a055a04
Trisha Bharti <trishabharti444@gmail.com>
2026-06-20 15:53:23 +0530

    final commit

--- NxtTale/NxtTaleApp.swift
+++ NxtTale/NxtTaleApp.swift
@@ -1,39 +1,4 @@
-//
-//  NxtTaleApp.swift
-//  NxtTale
-//
-//  Created by Vikram Kumar on 16/05/26.
-//
 
-//import SwiftUI
-//
-//@main
-//struct NxtTaleApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
-
-//import SwiftUI
-//
-//@main
-//struct NxtTaleApp: App {
-//    @StateObject var authVM = AuthViewModel()
-//
-//    var body: some Scene {
-//        WindowGroup {
-//            if authVM.isLoggedIn {
-//                MainTabView()
-//                    .environmentObject(authVM)
-//            } else {
-//                SignUpView()
-//                    .environmentObject(authVM)
-//            }
-//        }
-//    }
-//}
 
 import SwiftUI
 
